0180e81f40ebbf3562f74051116717fd0ebf3e51
Weijie Wang <duangsir518@163.com>
2024-06-05 19:42:40 +0200

    Revert "Merge branch 'main' into feature/centauro_wheel"
    This reverts commit 7920bf2587565a2ac5aff0235c2259c9a1ba9c75, reversing
    changes made to 83fd6f9e9904057222e3b4dd9fac1ca4de41941a.

--- build/catkin_generated/safe_execute_install.cmake
+++
@@ -1,5 +0,0 @@
-execute_process(COMMAND "/home/wang/forest_ws/src/centauro_long_task/build/catkin_generated/python_distutils_install.sh" RESULT_VARIABLE res)
-
-if(NOT res EQUAL 0)
-  message(FATAL_ERROR "execute_process(/home/wang/forest_ws/src/centauro_long_task/build/catkin_generated/python_distutils_install.sh) returned error code ")
-endif()
